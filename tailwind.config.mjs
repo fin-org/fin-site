afa46ab7243de11f674aabbef9d6854df83ad46e
Michael McClintock <mrmcc3@gmail.com>
2024-03-02 12:26:18 +1000

    radix colors and first section on /

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,53 @@
+import * as colors from "@radix-ui/colors";
+
+function colorScale(color) {
+  const scale = {};
+  for (const [key, value] of Object.entries(color)) {
+    scale[key.replace(/\D/g, "")] = value;
+  }
+  return scale;
+}
+
+const gray = colorScale(colors.gray);
+const grayDark = colorScale(colors.grayDark);
+const green = colorScale(colors.green);
+const greenDark = colorScale(colors.greenDark);
+const blue = colorScale(colors.blue);
+const blueDark = colorScale(colors.blueDark);
+const purple = colorScale(colors.purple);
+const purpleDark = colorScale(colors.purpleDark);
+const bronze = colorScale(colors.bronze);
+const bronzeDark = colorScale(colors.bronzeDark);
+const red = colorScale(colors.red);
+const redDark = colorScale(colors.redDark);
+const orange = colorScale(colors.orange);
+const orangeDark = colorScale(colors.orangeDark);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx}"],
   theme: {
+    colors: {
+      inherit: "inherit",
+      current: "currentColor",
+      transparent: "transparent",
+      black: "#000",
+      white: "#fff",
+      gray,
+      grayDark,
+      green,
+      greenDark,
+      blue,
+      blueDark,
+      purple,
+      purpleDark,
+      bronze,
+      bronzeDark,
+      red,
+      redDark,
+      orange,
+      orangeDark,
+    },
     extend: {
       fontFamily: {
         sans: [
