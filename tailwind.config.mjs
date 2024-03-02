87bf18c118b4a54a1be89772293792015906057e
Michael McClintock <mrmcc3@gmail.com>
2024-03-02 14:15:04 +1000

    start configuring markdoc blocks

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 import * as colors from "@radix-ui/colors";
+import typography from "@tailwindcss/typography";
 
 function colorScale(color) {
   const scale = {};
@@ -59,7 +60,51 @@ export default {
           { fontVariationSettings: '"MONO" 1' },
         ],
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": gray[11],
+            "--tw-prose-headings": gray[12],
+            "--tw-prose-lead": gray[10],
+            "--tw-prose-links": bronze[9],
+            "--tw-prose-bold": gray[12],
+            "--tw-prose-counters": gray[8],
+            "--tw-prose-bullets": gray[8],
+            "--tw-prose-hr": gray[6],
+            "--tw-prose-quotes": gray[12],
+            "--tw-prose-quote-borders": gray[6],
+            // '--tw-prose-captions': colors.gray[500],
+            // '--tw-prose-kbd': colors.gray[900],
+            // '--tw-prose-kbd-shadows': hexToRgb(colors.gray[900]),
+            "--tw-prose-code": gray[12],
+            "--tw-prose-pre-code": gray[12],
+            "--tw-prose-pre-bg": gray[3],
+            "--tw-prose-th-borders": gray[8],
+            "--tw-prose-td-borders": gray[6],
+
+            // invert
+            "--tw-prose-invert-body": grayDark[11],
+            "--tw-prose-invert-headings": grayDark[12],
+            "--tw-prose-invert-lead": grayDark[10],
+            "--tw-prose-invert-links": bronzeDark[9],
+            "--tw-prose-invert-bold": grayDark[12],
+            "--tw-prose-invert-counters": grayDark[8],
+            "--tw-prose-invert-bullets": grayDark[8],
+            "--tw-prose-invert-hr": grayDark[6],
+            "--tw-prose-invert-quotes": grayDark[12],
+            "--tw-prose-invert-quote-borders": grayDark[6],
+            // '--tw-prose-invert-captions': colors.gray[400],
+            // '--tw-prose-invert-kbd': colors.white,
+            // '--tw-prose-invert-kbd-shadows': hexToRgb(colors.white),
+            "--tw-prose-invert-code": grayDark[12],
+            "--tw-prose-invert-pre-code": grayDark[12],
+            "--tw-prose-invert-pre-bg": grayDark[3],
+            "--tw-prose-invert-th-borders": grayDark[8],
+            "--tw-prose-invert-td-borders": grayDark[6],
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [typography],
 };
